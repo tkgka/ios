422d35ca3f273f9e82504ebb0170c35b08cc6dc1
tkgka <52348220+tkgka@users.noreply.github.com>
2020-07-22 17:20:49 +0900

    .startChatApp

---
+++ ChatTest/ChatTest/Controllers/ConversationsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatTest
+//
+//  Created by 김수환 on 2020/07/22.
+//  Copyright © 2020 김수환. All rights reserved.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
