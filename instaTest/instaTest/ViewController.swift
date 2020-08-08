b52449fff79d0221a556d0d5f024f7b2940a2e59
tkgka <52348220+tkgka@users.noreply.github.com>
2020-08-08 22:20:42 +0900

    .서버에서 값 받아오기

--- instaTest/instaTest/ViewController.swift
+++ instaTest/instaTest/ViewController.swift
@@ -8,8 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+struct APIResponse: Codable {
+    let results: APIResponseResults
+    let status: String
+}
 
+struct APIResponseResults: Codable {
+    let sunrise: String
+    let sunset: String
+    let solar_noon: String
+    let day_length: Int
+    let civil_twilight_begin: String
+    let civil_twilight_end: String
+    let nautical_twilight_begin: String
+    let nautical_twilight_end: String
+    let astronomical_twilight_begin: String
+    let astronomical_twilight_end: String
+    
+}
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
     @IBOutlet var table: UITableView!
     
     var models = [InstagramPost]()
@@ -19,27 +38,72 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         fetchPostdata { (posts) in
             for post in posts {
                 self.models.append(InstagramPost(numberOfLikes: post.id, username: post.title, userImageName: "head", postImageName: "post_1"))
-             
+                
             }
         }
     }
     
     override func viewDidLoad() {
         append()
-        super.viewDidLoad()
         
+        super.viewDidLoad()
         table.register(PostTableViewCell.nib(), forCellReuseIdentifier: PostTableViewCell.identifier)
         table.delegate = self
         table.dataSource = self
-        
-        
-        
-        models.append(InstagramPost(numberOfLikes: 200, username: "hello", userImageName: "head", postImageName: "post_1"))
-        models.append(InstagramPost(numberOfLikes: 200, username: "hello", userImageName: "head", postImageName: "post_2"))
-        models.append(InstagramPost(numberOfLikes: 200, username: "hello", userImageName: "head", postImageName: "post_3"))
-        
-        
+        view.addSubview(table)
+        fetchData()
+        table.refreshControl = UIRefreshControl()
+        table.refreshControl?.addTarget(self, action: #selector(didPullToRefresh), for: .valueChanged)
     }
+    @objc private func didPullToRefresh() {
+        //refecth data
+        
+        fetchData()
+    }
+    
+    private func fetchData() {
+        models.removeAll()
+        
+        
+        if table.refreshControl?.isRefreshing == true {
+            print("refreshing data")
+        }else {
+            print("fetching data")
+        }
+        
+        guard let url = URL(string: "https://api.sunrise-sunset.org/json?data=2020-8-1&lng=37.3230&lat=-122.0322&formatted=0") else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url,completionHandler: {[weak self] data, _, error in
+            guard let strongself = self, let data = data, error == nil else {
+                return
+            }
+            var result: APIResponse?
+            do {
+                result = try JSONDecoder().decode(APIResponse.self, from: data)
+            }
+            catch
+            {
+                //handle error
+            }
+            guard let final = result else {
+                return
+            }
+            strongself.models.append(InstagramPost(numberOfLikes: 200, username: final.results.sunrise, userImageName: "head", postImageName: "post_1"))
+            //              strongself.models.append("Sunrise: \(final.results.sunrise)")
+            
+            
+            DispatchQueue.main.async {
+                strongself.table.refreshControl?.endRefreshing()
+                
+                strongself.table.reloadData()
+            }
+        })
+        task.resume()
+    }
+    
+    
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -61,28 +125,28 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         
     }
     
-   func fetchPostdata(completionHandler: @escaping ([Post]) -> Void){
-          
-          let url = URL(string: "https://jsonplaceholder.typicode.com/posts")!
-          
-          let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-              
-              guard let data = data else { return }
-              
-              do {
-                  
-                  let postsData = try JSONDecoder().decode([Post].self, from: data)
-                  
-                  completionHandler(postsData)
-                  
-              }catch{
-                  let error = error
-                  print("\(error.localizedDescription)")
-              }
-              
-          }.resume()
+    func fetchPostdata(completionHandler: @escaping ([Post]) -> Void){
+        
+        let url = URL(string: "https://jsonplaceholder.typicode.com/posts")!
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            guard let data = data else { return }
+            
+            do {
+                
+                let postsData = try JSONDecoder().decode([Post].self, from: data)
+                
+                completionHandler(postsData)
+                
+            }catch{
+                let error = error
+                print("\(error.localizedDescription)")
+            }
+            
+        }.resume()
     }
-
+    
 }
 
 struct InstagramPost {
