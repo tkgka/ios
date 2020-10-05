16a9594189a356bce5671774c33e48e784dd1b99
tkgka <52348220+tkgka@users.noreply.github.com>
2020-10-04 19:19:09 -0500

    .

---
+++ testApp/testApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  testApp
+//
+//  Created by 김수환 on 2020/10/01.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ testApp/testApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  testApp
+//
+//  Created by 김수환 on 2020/10/01.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
